8a6859fd8ecf26da73f4ba69f5dfb81cc9419b05
AliyevaMadina <mdn.alyv@gmail.com>
2024-08-08 14:50:58 +0400

    insert left fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,16 +12,23 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!parent->left)
-		new_node->left = NULL;
-	else
-	{
-			new_node->left = parent->left;
-			parent->left->parent = new_node;
-		}
+	if (!new_node)
+		return (NULL);
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->right = NULL;
+
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	else
+	{
+		new_node->left = NULL;
+	}
+
 	parent->left = new_node;
 
 	return (new_node);
