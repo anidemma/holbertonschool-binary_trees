0eb106280eceac1c39a3db32704591f38a7643e3
Your Name <you@example.com>
2024-08-10 15:34:16 +0400

    added

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - func
+ * @node: node
+ * Return: uncle of element
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
