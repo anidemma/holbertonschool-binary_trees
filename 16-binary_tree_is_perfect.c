6981a7478bb28c209d939f52e450b3d026a73096
Your Name <you@example.com>
2024-08-10 15:28:00 +0400

    betty fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,13 +8,13 @@ int isPerfect(const binary_tree_t *tree, int depth, int level);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int check, depth, level = 0;
-
-    if (tree == NULL)
-        return (0);
-    depth  = find_depth_left(tree);
-    check = isPerfect(tree, depth, level);
-    return (check); 
+	int check, depth, level = 0;
+	
+	if (tree == NULL)
+		return (0);
+	depth  = find_depth_left(tree);
+	check = isPerfect(tree, depth, level);
+	return (check); 
 }
 /**
  * find_depth_left - func
@@ -23,11 +23,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 int find_depth_left(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (!tree->left && !tree->right)
-        return (1);
-    return (1 + find_depth_left(tree->left));
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (1 + find_depth_left(tree->left));
 }
 /**
  * isPerfect - func
@@ -38,12 +38,12 @@ int find_depth_left(const binary_tree_t *tree)
  */
 int isPerfect(const binary_tree_t *tree, int depth, int level)
 {
-    if (!tree)
-        return (0);
-    if (!tree->left && !tree->right)
-        return (depth == (level + 1));  
-    if (!tree->left || !tree->right)
-        return (0);
-    return (isPerfect(tree->left, depth, level + 1) &&
-            isPerfect(tree->right, depth, level + 1));
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (depth == (level + 1));  
+	if (!tree->left || !tree->right)
+		return (0);
+	return (isPerfect(tree->left, depth, level + 1) &&
+		isPerfect(tree->right, depth, level + 1));
 }
