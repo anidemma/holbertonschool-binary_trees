ad721d31550282467e33cf4b1884eb432c9f26f2
Your Name <you@example.com>
2024-08-11 15:23:09 +0400

    100-binary_trees_ancestor.c added

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - func
+ * @first: var
+ * @second: var
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *i, *j;
+
+	if (!first || !second)
+		return (NULL);
+
+	i = (binary_tree_t *)first;
+
+	while (i)
+	{
+		j = (binary_tree_t *)second;
+		while (j)
+		{
+			if (i == j)
+				return (i);
+			j = j->parent;
+		}
+		i = i->parent;
+	}
+}
